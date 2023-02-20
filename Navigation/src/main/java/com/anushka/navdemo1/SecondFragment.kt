443b62e837fcf007e64a64ed239196cc25169636
yunjaem06 <yunjae0616@gmail.com>
2023-02-20 21:02:15 +0900

    navigation 실습

--- Navigation/src/main/java/com/anushka/navdemo1/SecondFragment.kt
+++ Navigation/src/main/java/com/anushka/navdemo1/SecondFragment.kt
@@ -22,6 +22,10 @@ class SecondFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
         viewModel = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
 
+        // 이 때 ViewModel 생성 시 Owner 파라미터에 ‘this’ 대신 ‘requireActivity()’ 를 사용해야된다.
+        // ‘this’를 사용한다면 두 프레그먼트는 서로 다른 Owner를 갖게 된다.
+        // 하지만 상위 액티비티를 Owner로 설정하는 requireActivity() 코드를 사용한다면 두 개 의 Fragment는 Owner로써 같은 Onwer (상위 액티비티)를 사용하게 된다.
+        // 이와 같이 같은 Onwer를 설정해 줌으로써 Fragment 간에 데이터를 공유할 수있다.
         viewModel.name.observe(requireActivity(), Observer {
             binding.textView.text = it.toString()
         })
