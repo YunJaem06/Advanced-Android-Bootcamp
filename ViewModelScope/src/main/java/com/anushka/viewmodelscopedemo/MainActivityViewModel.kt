526e3bfe5b4e4087d32f30cda221003dc8942adc
yunjaem06 <yunjae0616@gmail.com>
2023-03-01 20:34:41 +0900

    ViewModelScope 끝

--- ViewModelScope/src/main/java/com/anushka/viewmodelscopedemo/MainActivityViewModel.kt
+++ ViewModelScope/src/main/java/com/anushka/viewmodelscopedemo/MainActivityViewModel.kt
@@ -1,31 +1,24 @@
 package com.anushka.viewmodelscopedemo
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import androidx.lifecycle.viewModelScope
+import com.anushka.viewmodelscopedemo.model.User
+import com.anushka.viewmodelscopedemo.model.UserRepository
+import kotlinx.coroutines.*
 
 
 class MainActivityViewModel : ViewModel() {
+    private var userRepository = UserRepository()
+    var users : MutableLiveData<List<User>> = MutableLiveData()
 
-private val myJob = Job()
-private val myScope = CoroutineScope(Dispatchers.IO + myJob)
-
- fun getUserData(){
-
-     myScope.launch {
-         //write some code
-     }
-
- }
-
-    override fun onCleared() {
-        super.onCleared()
-        myJob.cancel()
-
-
+    fun getUserData(){
+        viewModelScope.launch {
+            var result : List<User>? = null
+            withContext(Dispatchers.IO){
+                result = userRepository.getUsers()
+            }
+            users.value = result!!
+        }
     }
-
-
 }
---
+++ ViewModelScope/src/main/java/com/anushka/viewmodelscopedemo/model/User.kt
@@ -0,0 +1,6 @@
+package com.anushka.viewmodelscopedemo.model
+
+data class User(
+    var id : Int,
+    var name : String
+)
