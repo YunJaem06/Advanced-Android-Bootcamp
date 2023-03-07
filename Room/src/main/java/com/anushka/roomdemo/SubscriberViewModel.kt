c6765f352db1f1b26ed8f11ae3372e7cdb635c07
yunjaem06 <yunjae0616@gmail.com>
2023-03-07 16:44:16 +0900

    유효성 검사하기

--- Room/src/main/java/com/anushka/roomdemo/SubscriberViewModel.kt
+++ Room/src/main/java/com/anushka/roomdemo/SubscriberViewModel.kt
@@ -1,5 +1,6 @@
 package com.anushka.roomdemo
 
+import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -36,17 +37,26 @@ class SubscriberViewModel(private val repository: SubscriberRepository) : ViewMo
     }
 
     fun saveOrUpdate() {
-        if (isUpdateOrDelete) {
-            subscriberToUpdateOrDelete.name = inputName.value!!
-            subscriberToUpdateOrDelete.email = inputEmail.value!!
-            update(subscriberToUpdateOrDelete)
-        } else {
-            val name = inputName.value!!
-            val email = inputEmail.value!!
-            insert(Subscriber(0, name, email))
 
-            inputName.value = ""
-            inputEmail.value = ""
+        if (inputName.value == null){
+            statusMessage.value = Event("Please enter subscriber`s name")
+        } else if (inputEmail.value == null){
+            statusMessage.value = Event("Please enter subscriber`s email")
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(inputEmail.value!!).matches()){
+            statusMessage.value = Event("Please enter a correct email address")
+        } else {
+            if (isUpdateOrDelete) {
+                subscriberToUpdateOrDelete.name = inputName.value!!
+                subscriberToUpdateOrDelete.email = inputEmail.value!!
+                update(subscriberToUpdateOrDelete)
+            } else {
+                val name = inputName.value!!
+                val email = inputEmail.value!!
+                insert(Subscriber(0, name, email))
+
+                inputName.value = ""
+                inputEmail.value = ""
+            }
         }
     }
 
