d2124d731bffdd05031f4ab4361de35403a694d9
yunjaem06 <yunjae0616@gmail.com>
2024-01-07 14:21:23 +0900

    feat - okhttp logging

--- Retrofit/src/main/java/com/anushka/retrofitdemo/MainActivity.kt
+++ Retrofit/src/main/java/com/anushka/retrofitdemo/MainActivity.kt
@@ -14,25 +14,22 @@ import retrofit2.Response
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var retService: AlbumService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val retService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+        retService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
 
-        val pathResponse : LiveData<Response<AlbumsItem>> = liveData {
-            var response = retService.getAlbum(3)
-            emit(response)
-        }
+        getRequestWithQueryParameters()
+//        getRequestWithPathParameters()
 
-        pathResponse.observe(this, Observer {
-            val title = it.body()?.title
-            Toast.makeText(applicationContext, title, Toast.LENGTH_LONG).show()
-        })
+    }
 
-
+    private fun getRequestWithQueryParameters() {
         val responseLiveData: LiveData<Response<Albums>> = liveData {
             val response = retService.getSortedAlbums(3)
             emit(response)
@@ -50,6 +47,18 @@ class MainActivity : AppCompatActivity() {
                     binding.tvList.append(result)
                 }
             }
+        })
+    }
+
+    private fun getRequestWithPathParameters() {
+        val pathResponse : LiveData<Response<AlbumsItem>> = liveData {
+            var response = retService.getAlbum(3)
+            emit(response)
+        }
+
+        pathResponse.observe(this, Observer {
+            val title = it.body()?.title
+            Toast.makeText(applicationContext, title, Toast.LENGTH_LONG).show()
         })
     }
 }
