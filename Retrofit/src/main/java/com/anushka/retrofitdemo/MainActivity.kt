3769ac542b17dd38c2743cccba9c6883b376fd58
yunjaem06 <yunjae0616@gmail.com>
2024-01-07 13:52:33 +0900

    feat - Retrofit get

--- Retrofit/src/main/java/com/anushka/retrofitdemo/MainActivity.kt
+++ Retrofit/src/main/java/com/anushka/retrofitdemo/MainActivity.kt
@@ -2,10 +2,42 @@ package com.anushka.retrofitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import com.anushka.retrofitdemo.databinding.ActivityMainBinding
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val retService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+
+        val responseLiveData: LiveData<Response<Albums>> = liveData {
+            val response = retService.getAlbums()
+            emit(response)
+        }
+
+        responseLiveData.observe(this, Observer {
+            val albumsList = it.body()?.listIterator()
+            if (albumsList != null) {
+                while (albumsList.hasNext()) {
+                    val albumsItem = albumsList.next()
+                    val result = " " + "Album Title : ${albumsItem.title}" + "\n" +
+                            " " + "Album id : ${albumsItem.id}" + "\n" +
+                            " " + "userid Title : ${albumsItem.userId}" + "\n\n\n"
+
+                    binding.tvList.append(result)
+                }
+            }
+        })
     }
 }
