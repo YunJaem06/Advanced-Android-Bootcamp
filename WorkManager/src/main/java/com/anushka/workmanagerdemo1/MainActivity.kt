25fe351809d9cf7c0515fbd7aa8530ba8c8aafc4
yunjaem06 <yunjae0616@gmail.com>
2024-01-27 12:40:10 +0900

    feat - Work Manager setInput, output

--- WorkManager/src/main/java/com/anushka/workmanagerdemo1/MainActivity.kt
+++ WorkManager/src/main/java/com/anushka/workmanagerdemo1/MainActivity.kt
@@ -2,14 +2,20 @@ package com.anushka.workmanagerdemo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.work.Constraints
+import androidx.work.Data
 import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val KEY_COUNT_VALUE = "key_count"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +28,11 @@ class MainActivity : AppCompatActivity() {
     private fun setOneTimeWorkRequest() {
 
         val workManager = WorkManager.getInstance(applicationContext)
+
+        val data : Data = Data.Builder()
+            .putInt(KEY_COUNT_VALUE, 125)
+            .build()
+
         val constraints = Constraints.Builder()
             .setRequiresCharging(true)
             .setRequiredNetworkType(NetworkType.CONNECTED)
@@ -29,11 +40,17 @@ class MainActivity : AppCompatActivity() {
 
         val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
             .setConstraints(constraints)
+            .setInputData(data)
             .build()
         workManager.enqueue(uploadRequest)
         workManager.getWorkInfoByIdLiveData(uploadRequest.id)
             .observe(this, Observer {
                 textView.text = it.state.name
+                if (it.state.isFinished) {
+                    val data = it.outputData
+                    val message = data.getString(UploadWorker.KEY_WORKER)
+                    Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+                }
             })
     }
 }
